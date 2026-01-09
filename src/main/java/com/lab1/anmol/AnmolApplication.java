0ba7f7f45033b7b69bdea050c205f2ecda9e9a60
Anmol10S <anmol10.singh2006@gmail.com>
2026-01-10 00:40:39 +0530

    Add files via upload

---
+++ src/main/java/com/lab1/anmol/AnmolApplication.java
@@ -0,0 +1,13 @@
+package com.lab1.anmol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnmolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnmolApplication.class, args);
+	}
+
+}
